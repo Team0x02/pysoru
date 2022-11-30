b9622b7a226140dd1286ef8e9c28d74d1dac8be6
readlydex0x02 <readlydex@protonmail.com>
2022-11-30 10:52:33 -0800

    Create pysoru.py

---
+++ pysoru.py
@@ -0,0 +1,173 @@
+print("SİBER GÜVENLİK SORULARI 2022-2023")
+print("""
+•
+•
+•""")
+
+print("""Aşağıdakilerden hangisi bir işletim sistemi değildir?
+ A) Linux
+ B) Windows
+ C) MacOS
+ D) UNIX
+ E) Meta""")
+soru1=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("""Aşağıdakilerden hangisi bir virüs türü değildir?
+ A) Trojan
+ B) Rootkit
+ C) Fidye Yazılımı
+ D) Brute Force
+ E) Reklam Yazılımı""")
+soru2=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("""Aşagıdakiler Hangisi Makina diline yakındır
+ (A Ruby
+ (B Assambly
+ (C C#
+ (D C++
+ (E Python""")
+soru3=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("""Telefon çeviriciler (dialer)
+Arka kapılar (backdoor)
+Mesaj sağanakları (spam)
+Kök kullanıcı takımları (rootkit)
+•-----•
+Yukarıdakilerden hangisi veya hangileri en genel kötücül yazılımlardandır?
+A-) Yalnız III
+
+B-) II ve III
+
+C-) III ve IV
+
+D-) I ve IV
+
+E-) Yalnız I""")
+soru4=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("""Mail içerisinde kötü niyetli bir link ya da doküman barındırır ve kullanıcıların bilgilerini çalmayı hedefler”. Bu cümleyi tanımlayan seçenek hangisidir?
+A) Zararlı yazılım
+
+B) DDoS
+
+C) Parola saldırıları
+
+D) Phishing
+
+E) Truva atı""")
+soru5=input("Cevabınız : ")
+print("""
+•
+•
+•""")
+print("""Verileri bilgisayarda şifreli olarak tutmak için hangi program kullanılır.
+A) HD Tune UP
+
+B) Norton Security
+
+C) Bitlocker
+
+D) Furmark
+
+E) Root""")
+soru6=input
+print("""
+•
+•
+•""")
+print("""Aşağıdakilerden hangisi siber suça örnek teşkil etmez?
+A) Sibirya doğalgaz patlaması
+
+B) Ay ışığı labirenti
+
+C) Çernobil olayı
+
+D) Mavi Marmara Saldırısı
+
+E) NATO Kosova krizi""")
+soru7=input
+print("""
+•
+•
+•""")
+print("""Bilginin elde edilmesinde aşağıdakilerden hangisi informal kaynaklar arasındadır?
+A) Aktivite gözlemlenmesi
+
+B) İş formları
+
+C) Bilgi veri tabanları
+
+D) Personel kayıtları
+
+E) Hiçbiri""")
+soru8=input
+print("""
+•
+•
+•""")
+print("""TSE-17799 “Bilgi Güvenliği Yönetim Standardı” belgesine göre aşağıdakilerden hangisi bilgi güvenliğinin temel unsurları arasında değildir?
+A) Bütünlük
+
+B) İnkar etme
+
+C) Kimlik doğrulama
+
+D) Sorumluluk
+
+E) Hepsi""")
+soru9=input
+print("""
+•
+•
+•""")
+print(""" Hedef makinenin internet erişimini bir vekil sunucu gibi saldırganın hizmetine açan Truva nedir?
+
+A)Truva casusları
+
+B) Truva Vekilleri
+
+C) Truva Damlalıkları
+
+D) Truva indiriciler
+
+E) Truva Tıklayıcılar""")
+soru10=input
+print("""
+•
+•
+•
+SORULAR BİTMİŞTİR CEVAPLAR ALT KISIMDADIR.
+•
+•
+•
+""")
+print("Cevaplarınız  : ")
+print("""Cevaplar
+  1:E
+  2:D
+  3:B
+  4:C
+  5:C
+  6:C
+  7:C
+  8:A
+  9:B
+ 10:B
+""")
+print("SİBER SORULAR BİTMİŞTİR YENİ BİR MARATONDA GÖRÜŞÜRÜZ")
+print("""INSTAGRAM
+ @ghost0x02
+ @readlydex0x02.asm
+FİKİR VE GÖRÜŞLERİNİZ İÇİN VERİLEN HESAPLARA YAZMANIZ YETERLİDİR.""")
